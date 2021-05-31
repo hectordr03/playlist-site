db955be8dff07b182f411a18a3a06a5e0877e03f
Hectordr03 <Hectordr03@gmail.com>
2021-05-31 07:17:53 -0400

    convert to portfolio site

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import style from '../styles/index.module.css';
 const Home = () => {
 	return (
 		<div className={style.home}>
-			<h1 className={style.title}>Welcome to my playlist</h1>
+			
 		</div>
 	);
 };
