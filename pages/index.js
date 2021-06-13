26d3f58b88b52b97db24b8e17f7c9d9e939f4adc
Hectordr03 <Hectordr03@gmail.com>
2021-06-13 11:28:45 -0400

    trouble centering ship png and resizing

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,11 @@ import style from '../styles/index.module.css';
 
 const Home = () => {
 	return (
+	<div>
 		<div className={style.home}>
-			
+		<div className={style.ship}></div>
 		</div>
+	</div>
 	);
 };
 
